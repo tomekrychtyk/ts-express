cc0a73d845d72e4bb901202841def3a4cbc95bd0
Tomek Rychtyk <tomek@rychtyk.pl>
2022-07-20 00:56:45 +0200

    Controller and routes decorators

--- server/src/controllers/decorators/controller.ts
+++ server/src/controllers/decorators/controller.ts
@@ -1,11 +1,28 @@
 import "reflect-metadata";
+import { AppRouter } from "../../appRouter";
+import { Methods } from "./Methods";
+import { MetadataKeys } from "./MetadataKeys";
 
 export function controller(routePrefix: string) {
+  const router = AppRouter.getInstance();
+
   return function (target: Function) {
     for (let key in target.prototype) {
-      const routeHanlder = target.prototype[key];
+      const routeHandler = target.prototype[key];
+      const path = Reflect.getMetadata(
+        MetadataKeys.path,
+        target.prototype,
+        key
+      );
+      const method: Methods = Reflect.getMetadata(
+        MetadataKeys.method,
+        target.prototype,
+        key
+      );
 
-      const path = Reflect.getMetadata("path", target.prototype, key);
+      if (path) {
+        router[method](`${routePrefix}${path}`, routeHandler);
+      }
     }
   };
 }
